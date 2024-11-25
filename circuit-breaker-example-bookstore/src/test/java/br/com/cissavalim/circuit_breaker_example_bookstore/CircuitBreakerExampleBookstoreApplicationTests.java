368e34f7e9d0005748ecced31ffcc5a5b4828e23
Cecilia Valim <cissamadalenarv@gmail.com>
2024-11-24 22:39:30 -0300

    adding client and server applications

---
+++ circuit-breaker-example-bookstore/src/test/java/br/com/cissavalim/circuit_breaker_example_bookstore/CircuitBreakerExampleBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.circuit_breaker_example_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerExampleBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ circuit-breaker-example-reading/src/test/java/br/com/cissavalim/circuit_breaker_example_reading/CircuitBreakerExampleReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.circuit_breaker_example_reading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerExampleReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
